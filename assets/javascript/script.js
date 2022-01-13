4f03605fc57c5b01dfac22dc8a048aedb3fef095
antieatingactivist <antieatingactivist@gmail.com>
2022-01-13 13:47:37 -0800

    changed layout

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,7 +14,7 @@ function clearArtist() {
     if(left.innerHTML !== '' && right.innerHTML !== ''){
         left.innerHTML= '';
         right.innerHTML= '';
-        albumCarousel.refresh(true);
+        
     } else {
         searchArtist;
     }
@@ -177,58 +177,26 @@ function createImg(name) {
 
 var albumCarousel = new Glider(document.querySelector('.glider'), {
 
-    // `auto` allows automatic responsive
-    // width calculations
     slidesToShow: 'auto',
     slidesToScroll: 'auto',
-  
-    // should have been named `itemMinWidth`
-    // slides grow to fit the container viewport
-    // ignored unless `slidesToShow` is set to `auto`
     itemWidth: undefined,
-  
-    // if true, slides wont be resized to fit viewport
-    // requires `itemWidth` to be set
-    // * this may cause fractional slides
     exactWidth: false,
-  
-    // speed aggravator - higher is slower
     duration: .5,
-  
-    // dot container element or selector
     dots: '.dots',
-  
-    // arrow container elements or selector
     arrows: {
         prev: '.glider-prev',
         next: '.glider-next'
     },
-  
-    // allow mouse dragging
     draggable: false,
-    // how much to scroll with each mouse delta
     dragVelocity: 3.3,
-  
-    // use any custom easing function
-    // compatible with most easing plugins
     easing: function (x, t, b, c, d) {
       return c*(t/=d)*t + b;
     },
-  
-    // event control
     scrollPropagate: false,
     eventPropagate: true,
-  
-    // Force centering slide after scroll event
     scrollLock: false,
-    // how long to wait after scroll event before locking
-    // if too low, it might interrupt normal scrolling
     scrollLockDelay: 150,
-  
-    // Force centering slide after resize event
     resizeLock: true,
-  
-    // Glider.js breakpoints are mobile-first
     responsive: [
       {
         breakpoint: 900,
